93938b72fa1b967f6d720976d7577143b2d23dc0
Fernando Barbosa <fernando.barbosa@walmart.com>
2018-03-26 14:37:13 -0300

    add start class

---
+++ src/main/java/com/netss/supporter/SupporterApplication.java
@@ -0,0 +1,12 @@
+package com.netss.supporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupporterApplication.class, args);
+	}
+}
