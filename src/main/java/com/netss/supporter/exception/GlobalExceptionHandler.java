0bb869b1b0fa497d0a0bb374d2e33d049fded024
Fernando Barbosa <fernando.barbosa@walmart.com>
2018-03-26 23:41:37 -0300

    handle generic error messages

--- src/main/java/com/netss/supporter/exception/GlobalExceptionHandler.java
+++ src/main/java/com/netss/supporter/exception/GlobalExceptionHandler.java
@@ -1,17 +1,39 @@
 package com.netss.supporter.exception;
 
+import com.netss.supporter.exception.helper.ApiError;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.sql.SQLException;
 
 //TODO: user=fh message='improve exception handling'
 @ControllerAdvice
-public class GlobalExceptionHandler  {
+public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = { Exception.class })
-    protected ResponseEntity<String> generalHandler() {
-        String bodyOfResponse = "An Internal error has ocurred. Please contact support for more information.";
-        return new ResponseEntity<>(bodyOfResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    private final String GENERAL_ERROR_MESSAGE = "An Internal error has occurred. Please contact support for more information.";
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> generalHandler(Exception ex, WebRequest request) {
+        return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, GENERAL_ERROR_MESSAGE));
+    }
+
+    @ExceptionHandler({SQLException.class})
+    public ResponseEntity<Object> databaseError(SQLException sqlEx, WebRequest request) {
+        return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, GENERAL_ERROR_MESSAGE));
+    }
+
+    @ExceptionHandler(value = {DataIntegrityViolationException.class})
+    protected ResponseEntity<Object> handleDataIntegrityConstraintViolation(
+        DataIntegrityViolationException ex, WebRequest request) {
+        return buildResponseEntity(new ApiError(HttpStatus.CONFLICT, GENERAL_ERROR_MESSAGE));
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 }
---
+++ src/main/java/com/netss/supporter/exception/helper/RestExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.netss.supporter.exception.helper;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    public static final String MALFORMED_JSON_REQUEST = "Malformed JSON request";
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = MALFORMED_JSON_REQUEST;
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+    protected ResponseEntity<Object> handleEntityNotFound(
+        EntityNotFoundException ex) {
+
+        ApiError apiError = new ApiError(HttpStatus.NOT_FOUND);
+        apiError.setMessage(ex.getMessage());
+
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    protected ResponseEntity<Object> handleConstraintViolation(
+        ConstraintViolationException ex,
+        WebRequest request) {
+
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, ex));
+    }
+
+}
